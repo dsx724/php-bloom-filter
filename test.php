46de34477c23f3dea78073e7f2636b59eb7529d3
Da Xue <dx@xtrmm.(none)>
2012-08-27 13:30:37 -0400

    Improve Test
    Signed-off-by: Da Xue <dx@xtrmm.(none)>

--- test.php
+++ test.php
@@ -1,46 +1,49 @@
 <pre>
 <?php
 ini_set('display_errors','on');
+ini_set('memory_limit','32G');
+ini_set('max_execution_time',600);
+error_reporting(E_ALL);
+ini_set('display_errors',1);
+set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+	die($errno.': '.$errstr.' in '.$errfile.' on '.$errline);
+});
+
 require_once 'bloom-filter.php';
 
-for ($i = 1; $i < 8; $i++){
-	for ($j = 0.5; $j > 0.000001; $j /= 2){
+for ($i = 1; $i < 6; $i++){
+	for ($j = 0.1; $j > 0.000001; $j /= 10){
 		$n = (int)pow(10,$i);
 		$bf = BloomFilter::createFromProbability($n, $j);
 		echo 'N: '.$n."\tP: ".$j;
-		echo $bf->calculateCapacity($j) < $n ? PHP_EOL.$bf->getInfo($j) : ' OK';
+		echo $bf->calculateCapacity($j) < $n ? PHP_EOL.$bf->getInfo($j) : ' OK ';
+		for ($k = 0; $k < $n; $k++) $bf->add('T'.$k*3);
+		$false_neg = 0;
+		$false_pos = 0;
+		for ($k = 0; $k < $n*3; $k++) {
+			if ($k % 3 == 0) $false_neg += !$bf->check('T'.$k);
+			else $false_pos += $bf->check('T'.$k);
+		}
+		echo ' '.$false_neg.' '.$false_pos.' '.number_format($false_pos / $n / 3,2).' '.($false_pos / $n / 3 < $j && $false_neg == 0) ? 'PASS' : 'FAIL';
 		echo PHP_EOL;
 	}
 }
-for ($i = 3; $i < 24; $i++){
-	for ($j = 0.5; $j > 0.000001; $j /= 2){
+for ($i = 3; $i < 14; $i++){
+	for ($j = 0.1; $j > 0.000001; $j /= 10){
 		$n = (int)pow(2,$i);
 		$bf = BloomFilter::createFromProbability($n, $j);
 		echo 'N: '.$n."\tP: ".$j;
 		echo $bf->calculateCapacity($j) < $n ? PHP_EOL.$bf->getInfo($j) : ' OK';
+		for ($k = 0; $k < $n; $k++) $bf->add('T'.$k*3);
+		$false_neg = 0;
+		$false_pos = 0;
+		for ($k = 0; $k < $n*3; $k++) {
+			if ($k % 3 == 0) $false_neg += !$bf->check('T'.$k);
+			else $false_pos += $bf->check('T'.$k);
+		}
+		echo ' '.$false_neg.' '.$false_pos.' '.number_format($false_pos / $n / 3,2).' '.($false_pos / $n / 3 < $j && $false_neg == 0) ? 'PASS' : 'FAIL';
 		echo PHP_EOL;
 	}
 }
-die();
-
-//$bf1 = BloomFilter::createFromProbability(100000000, 0.01);
-$n = 10000000;
-$p = 0.01;
-echo 'Asking for '.number_format($n).' elements with a probability of '.$p.PHP_EOL;
-$bf1 = BloomFilter::createFromProbability($n, $p);
-echo $bf1->getInfo(0.01);
-//echo $bf1->calculateProbability(26).PHP_EOL;
-$max = 100000;
-for ($i = 0; $i < $max; $i+=2) $bf1->add('Test'.$i);
-
-$start1 = microtime(true);
-
-for ($i = $max; $i > 0; $i--) $bf1->check('Test'.$i);
-
-$end1 = microtime(true);
-$elapsed1 = $end1 - $start1;
-printf('%10.10f',$elapsed1);
-
-
 ?>
 </pre>
