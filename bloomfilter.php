91d1579930803e303bf2b384b875317d37fb27cb
dsx724 <da@lessconfused.com>
2013-05-06 18:31:31 -0300

    BSD License

--- bloomfilter.php
+++ bloomfilter.php
@@ -10,10 +10,7 @@ modification, are permitted provided that the following conditions are met:
 2. Redistributions in binary form must reproduce the above copyright
    notice, this list of conditions and the following disclaimer in the
    documentation and/or other materials provided with the distribution.
-3. All advertising materials mentioning features or use of this software
-   must display the following acknowledgement:
-   This product includes software developed by Da Xue.
-4. The name of the author nor the names of its contributors may be used 
+3. The name of the author nor the names of its contributors may be used 
    to endorse or promote products derived from this software without 
    specific prior written permission.
 
